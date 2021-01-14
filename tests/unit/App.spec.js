a2cd67401ae69f232974684f44e81b265886e731
Kristian Muñoz <kristian.munoz@codurance.com>
2021-01-14 16:34:15 +0100

    Clean test

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -2,11 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/pages/index'
 
 test('Should render homepage with NUXT text.', () => {
-    const app = shallowMount(App, {
-        propsData: {
-            results: []
-        }
-    })
+    const app = shallowMount(App, {})
 
     expect(app.text()).toContain('NUXT - Search images from NASA:')
 })
