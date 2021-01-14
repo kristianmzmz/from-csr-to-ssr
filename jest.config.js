6affdbf9b61979fd7041f6c26c6d61fce08f25df
Kristian Muñoz <kristian.munoz@codurance.com>
2021-01-14 16:32:30 +0100

    Add jest config and remove older VUE structure

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'vue',
+    'json'
+  ],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  }
 }
